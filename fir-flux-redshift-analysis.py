f6cbe7ff82389711adf0451e4a20340e4f282c7a
Claudia Lagos <claudia.lagos@icrar.org>
2019-10-11 18:49:43 +0800

    Updating FIR analysis for paper II of SEDs

--- fir-flux-redshift-analysis.py
+++ fir-flux-redshift-analysis.py
@@ -30,6 +30,22 @@ import os
 flux_threshs = np.array([1e-3, 1e-2, 2.5e-2, 5e-2, 7.5e-2, 1e-1, 2.5e-1, 3.5e-1, 4.5e-1, 5.5e-1, 6.5e-1, 7.5e-1, 8.5e-1, 9.5e-1, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0]) #in milli Jy
 flux_threshs_log = np.log10(flux_threshs[:])
 
+flux_threshs_compact = np.array([1e-2, 1e-1, 1.0])
+flux_threshs_compactab = -2.5 * np.log10(flux_threshs_compact*1e-3 / 3631.0)
+
+mlowab = 0
+muppab = 30
+dmab = 0.5
+mbinsab = np.arange(mlowab,muppab,dmab)
+xlfab   = mbinsab + dmab/2.0
+
+zlow = 0
+zupp = 7.0
+dz = 0.25
+zbins = np.arange(zlow,zupp,dz)
+xz   = zbins + dz/2.0
+
+
 # Constants
 GyrToYr = 1e9
 Zsun = 0.0127
@@ -37,9 +53,9 @@ XH = 0.72
 PI = 3.141592654
 MpcToKpc = 1e3
 c_light = 299792458.0 #m/s
+h0 = 0.6751
 
 # Mass function initialization
-
 mlow = -2.0
 mupp = 3.0
 dm = 0.25
@@ -80,6 +96,7 @@ def plot_observations(ax):
     ax.plot(3.6, 2.3, 'x', mec='aqua',markersize=9,mew=3)  # Simpson+14
     ax.plot(8.0, 2.65, 'x', mec='aqua',markersize=9,mew=3) # Simpson+17
     ax.plot(2.25, 2.74, 'x', mec='aqua',markersize=9, mew=3)    # Cowie+18
+    ax.plot(5.0, 2.8, 'x', mec='aqua',markersize=9, mew=3)    # Dudzeviciute+19 
     ax.plot(3.5, 2.48, 'x', mec='green',markersize=9,mew=3)  # Brisbin+17
     ax.plot(3.3, 3.1, 'x', mec='green',markersize=9,mew=3) # Miettinen15 as revised by Brisbin17
     ax.plot(25.0, 3.9, 's', mfc='none',mec='orange',markersize=9,mew=3) # Strandet+16
@@ -174,6 +191,7 @@ def plot_redshift(plt, outdir, obsdir, zdist_flux_cuts, zdist_flux_cuts_scatter)
             ax.plot(3.6, 2.3, 'D', mec='CadetBlue',markersize=9,mew=2)  # Simpson+14
             ax.plot(8.0, 2.65, 'D', mec='CadetBlue',markersize=9,mew=2) # Simpson+17
             ax.plot(2.25, 2.74, 'D', mec='CadetBlue',markersize=9, mew=2)    # Cowie+18
+            ax.plot(5.0, 2.8, 'D', mec='CadetBlue',markersize=9, mew=3)    # Dudzeviciute+19 
         elif (p == 4):
             ax.plot(4.2, 3.1, 'D', mec='green',markersize=9,mew=3) #Smolcic+12
             ax.plot(1.0, 2.2, 'o', mfc='green',markersize=9) #Michalowski+12
@@ -185,17 +203,232 @@ def plot_redshift(plt, outdir, obsdir, zdist_flux_cuts, zdist_flux_cuts_scatter)
 
         common.prepare_legend(ax, 'k', loc='upper left')
         p = p + 1
-
   
     plt.tight_layout()  # This makes sure labels dont go off page
     common.savefig(outdir, fig, "zdistribution-fluxcut-AllIRbands-splitpanels.pdf")
 
-def prepare_data(phot_data, ids_sed, hdf5_data, subvols, lightcone_dir, nbands, bands, zdist_flux_cuts, zdist_flux_cuts_scatter):
 
-    (dec, ra, zobs, idgal) = hdf5_data
+def plot_number_counts_smgs(plt, outdir, obsdir,  ncounts_optical_ir_smgs, bands_of_interest_for_smgs, ncounts_all):
+
+
+    def plot_number_counts(alma_band, name):
+        xlf_obs  = xlfab
+  
+        xtit="$\\rm app\, mag (AB)$"
+        ytit="$\\rm log_{10}(N/dex/A\, deg^2)$"
  
+        xmin, xmax, ymin, ymax = 10, 30, -2 , 5.5
+        xleg = xmax - 0.1 * (xmax-xmin)
+        xleg2 = xmin + 0.2 * (xmax-xmin)
+        yleg = ymax - 0.1 * (ymax-ymin)
+ 
+        fig = plt.figure(figsize=(12,12))
+ 
+        subplots = (331, 332, 333, 334, 335, 336, 337, 338, 339)#, 5510, 5511, 5512, 5513, 5514, 5515, 5516, 5517, 5518, 5519, 5520, 5521, 5522, 5523, 5524, 5525)
+        idx = (0, 1, 2, 3, 4, 5, 6, 7, 8)#, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24)
+        labels= ('u', 'g', 'r', 'J', 'H', 'K', '$4.6\\mu m$', '$12\\mu m$', '$22\\mu m$')
+        #"FUV_GALEX", "NUV_GALEX", "u_SDSS", "g_SDSS", "r_SDSS", "i_SDSS",
+        #"z_SDSS", "Y_VISTA", "J_VISTA", "H_VISTA", "K_VISTA", "W1_WISE",
+        #"W2_WISE", "W3_WISE", "W4_WISE"
+        #bands_of_interest_for_smgs = (0,1,2,3,4,5,6,7,8,10,11,14,17,18)
+        bands = (2, 3, 4, 8, 9, 10, 12, 13, 14)
+        labelsflux = ('>0.01mJy', '>0.1mJy', '>1mJy')
+        colors = ('Indigo','YellowGreen','Orange')
+  
+        for subplot, idx, b in zip(subplots, idx, bands):
+ 
+            ax = fig.add_subplot(subplot)
+            if (idx == 0 or idx == 3 or idx == 6):
+                ytitplot = ytit
+            else:
+                ytitplot = ' '
+            if (idx >= 6):
+                xtitplot = xtit
+            else:
+                xtitplot = ' '
+            common.prepare_ax(ax, xmin, xmax, ymin, ymax, xtitplot, ytitplot, locators=(2, 2, 1, 1))
+            if (idx == 0):
+                ax.text(xleg,yleg, labels[idx])
+            else:
+                ax.text(xleg2,yleg, labels[idx])
+ 
+            if (idx == 1):
+               ax.text(15,5.7,name,fontsize=17)    
+ 
+            for f in range(0,len(labelsflux)):
+                #Predicted LF
+                ind = np.where(ncounts_all[b,:] != 0)
+                y = ncounts_all[b,ind]
+                ax.plot(xlf_obs[ind],y[0],linewidth=3, linestyle='solid', color='grey')
+                ind = np.where(ncounts_optical_ir_smgs[alma_band,b,f,:] != 0)
+                y = ncounts_optical_ir_smgs[alma_band,b,f,ind] 
+                if(idx == 0):
+                    ax.plot(xlf_obs[ind],y[0],linewidth=3, linestyle='solid', color=colors[f], label=labelsflux[f])
+        
+                if(idx > 0):
+                    ax.plot(xlf_obs[ind],y[0],linewidth=3, linestyle='solid', color=colors[f])
+ 
+            if (idx == 0):
+                common.prepare_legend(ax, colors, loc='upper left')
+
+        namefig = "number-counts-smgs-" + name + ".pdf"
+        common.savefig(outdir, fig, namefig)
+
+
+    #Define bands
+    alma_bands = [0, 1, 2]
+    names_bands = ['Band-7-selected', 'Band-6-selected', 'Band-4-selected']
+
+    #plot number counts for all ALMA band selected galaxies
+    for j in alma_bands:
+        plot_number_counts(j, names_bands[j])
+
+def plot_magnitudes_z_smgs(plt, outdir, obsdir, mags_vs_z_flux_cuts):
+
+    def plot_mags(alma_band, name):
+        xlf_obs  = xlfab
+  
+        xtit="$\\rm redshift$"
+        ytit="$\\rm AB\\,  mag$"
+ 
+        xmin, xmax, ymin, ymax = 0, 6, 12, 35
+        xleg = xmax - 0.18 * (xmax-xmin)
+        yleg = ymin + 0.1 * (ymax-ymin)
+ 
+        fig = plt.figure(figsize=(12,10))
+ 
+        subplots = (331, 332, 333, 334, 335, 336, 337, 338, 339)#, 5510, 5511, 5512, 5513, 5514, 5515, 5516, 5517, 5518, 5519, 5520, 5521, 5522, 5523, 5524, 5525)
+        idx = (0, 1, 2, 3, 4, 5, 6, 7, 8)#, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24)
+        labels= ('u', 'g', 'r', 'J', 'H', 'K', '$4.6\\mu m$', '$12\\mu m$', '$22\\mu m$')
+        #"FUV_GALEX", "NUV_GALEX", "u_SDSS", "g_SDSS", "r_SDSS", "i_SDSS",
+        #"z_SDSS", "Y_VISTA", "J_VISTA", "H_VISTA", "K_VISTA", "W1_WISE",
+        #"W2_WISE", "W3_WISE", "W4_WISE"
+        #bands_of_interest_for_smgs = (0,1,2,3,4,5,6,7,8,10,11,14,17,18)
+        bands = (2, 3, 4, 8, 9, 10, 12, 13, 14)
+        labelsflux = ('>0.01mJy', '>0.1mJy', '>1mJy')
+        colors = ('Indigo','Green','Orange')
+  
+        for subplot, idx, b in zip(subplots, idx, bands):
+ 
+            ax = fig.add_subplot(subplot)
+            if (idx == 0 or idx == 3 or idx == 6):
+                ytitplot = ytit
+            else:
+                ytitplot = ' '
+            if (idx >= 6):
+                xtitplot = xtit
+            else:
+                xtitplot = ' '
+            common.prepare_ax(ax, xmin, xmax, ymin, ymax, xtitplot, ytitplot, locators=(2, 2, 3, 3))
+            ax.text(xleg,yleg, labels[idx])
+ 
+            if (idx == 1):
+               ax.text(1.5,36,name,fontsize=17)    
+ 
+            for f in range(0,len(labelsflux)):
+                #Predicted LF
+                ind = np.where(mags_vs_z_flux_cuts[alma_band,b,f,0,:] != 0)
+                y = mags_vs_z_flux_cuts[alma_band,b,f,0,ind]    
+                yerrdn = y - mags_vs_z_flux_cuts[alma_band,b,f,1,ind] 
+                yerrup = y + mags_vs_z_flux_cuts[alma_band,b,f,2,ind] 
+                ax.fill_between(xz[ind],yerrdn[0], yerrup[0], facecolor=colors[f], alpha=0.2,interpolate=True)
+
+                if(idx == 0):
+                    ax.plot(xz[ind],y[0],linewidth=3, linestyle='solid', color=colors[f], label=labelsflux[f])
+        
+                if(idx > 0):
+                    ax.plot(xz[ind],y[0],linewidth=3, linestyle='solid', color=colors[f])
+
+ 
+            if (idx == 0):
+                common.prepare_legend(ax, colors, loc='lower left')
+
+
+        namefig = "mag_vs_redshift-smgs-" + name + ".pdf"
+        common.savefig(outdir, fig, namefig)
+
+
+    #Define bands
+    alma_bands = [0, 1, 2]
+    names_bands = ['Band-7-selected', 'Band-6-selected', 'Band-4-selected']
+
+    #plot number counts for all ALMA band selected galaxies
+    for j in alma_bands:
+        plot_mags(j, names_bands[j])
+
+def plot_props_z_smgs(plt, outdir, obsdir, props_vs_z_flux_cuts, ms_z):
+
+    xtit="$\\rm redshift$"
+    ytits=["$\\rm log_{\\rm 10}(M_{\\star}/M_{\\odot})$", "$\\rm log_{\\rm 10}(sSFR/Gyr^{-1})$", "$\\rm log_{\\rm 10}(M_{\\rm halo}/M_{\\odot})$"]
+    xmin, xmax = 0, 6 
+    ymin = [8, -1, 10]
+    ymax = [12, 2, 14]
+
+    xleg = xmax - 0.7 * (xmax-xmin)
+ 
+    fig = plt.figure(figsize=(12,10))
+ 
+    subplots = (331, 332, 333, 334, 335, 336, 337, 338, 339)#, 5510, 5511, 5512, 5513, 5514, 5515, 5516, 5517, 5518, 5519, 5520, 5521, 5522, 5523, 5524, 5525)
+    idx = (0, 1, 2, 3, 4, 5, 6, 7, 8)#, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24)
+    names_bands = ['Band-7-selected', 'Band-6-selected', 'Band-4-selected']
+
+    labelsflux = ('>0.01mJy', '>0.1mJy', '>1mJy')
+    colors = ('Indigo','Green','Orange')
+  
+    p = 0
+    for prop in range(0,3):
+
+        if (p >= 6):
+            xtitplot = xtit
+        else:
+            xtitplot = ' '
+
+        yleg = ymax[prop] - 0.1 * (ymax[prop]-ymin[prop])
+
+        for b in range(0,3):
+            ax = fig.add_subplot(subplots[p])
+            if b == 0:
+               ytitplot = ytits[prop]
+            else:
+               ytitplot = ' '
+
+            common.prepare_ax(ax, xmin, xmax, ymin[prop], ymax[prop], xtitplot, ytitplot, locators=(2, 2, 0.5, 0.5))
+            ax.text(xleg,yleg, names_bands[b])
+            if(prop == 1):
+                ind = np.where(ms_z[0,:] != 0)
+                y = ms_z[0,ind]
+                ax.plot(xz[ind],y[0], linestyle='dotted', color='k', linewidth=5)
+
+            for f in range(0,len(labelsflux)):
+                #Predicted LF
+                ind = np.where(props_vs_z_flux_cuts[b,prop,f,0,:] != 0)
+                y = props_vs_z_flux_cuts[b,prop,f,0,ind]
+                yerrdn = y - props_vs_z_flux_cuts[b,prop,f,1,ind] 
+                yerrup = y + props_vs_z_flux_cuts[b,prop,f,2,ind] 
+                ax.fill_between(xz[ind],yerrdn[0], yerrup[0], facecolor=colors[f], alpha=0.2,interpolate=True)
+                if(p == 2):
+                    ax.plot(xz[ind],y[0],linewidth=3, linestyle='solid', color=colors[f], label=labelsflux[f])
+                else:
+                    ax.plot(xz[ind],y[0],linewidth=3, linestyle='solid', color=colors[f])
+            if (p == 2):
+                common.prepare_legend(ax, colors, loc='lower left')
+
+            p = p + 1
+
+    namefig = "props_vs_redshift-smgs.pdf"
+    common.savefig(outdir, fig, namefig)
+
+
+def prepare_data(phot_data, ids_sed, hdf5_data, subvols, lightcone_dir, nbands, bands, zdist_flux_cuts, 
+                 zdist_flux_cuts_scatter, ncounts_optical_ir_smgs, bands_of_interest_for_smgs, selec_alma,
+                 ncounts_all, mags_vs_z_flux_cuts, props_vs_z_flux_cuts, ms_z):
+
+    (dec, ra, zobs, idgal, msb, msd, mhalo, sfrb, sfrd, typeg) = hdf5_data
+
     bin_it = functools.partial(us.medians_cum_err, xbins=flux_threshs_log)
     bin_it_scatter = functools.partial(us.wmedians_cum, xbins=flux_threshs_log)
+
+    bin_it_z = functools.partial(us.wmedians, xbins=xz)
 
     #components of apparent magnitudes:
     #(len(my_data), 2, 2, 5, nbands)
@@ -210,7 +443,6 @@ def prepare_data(phot_data, ids_sed, hdf5_data, subvols, lightcone_dir, nbands, 
     SEDs_dust_bulge_d = phot_data[3]
     SEDs_dust_bulge_m = phot_data[4]
 
-    #print SEDs_dust(0)
     indices = range(len(bands))
     for i, j in zip(bands, indices):
         #calculate number counts for total magnitude as well as no dust magnitudes
@@ -218,6 +450,35 @@ def prepare_data(phot_data, ids_sed, hdf5_data, subvols, lightcone_dir, nbands, 
         m = np.log10(10.0**(SEDs_dust[i,ind]/(-2.5))*3631.0*1e3) #in mJy
         zdist_flux_cuts[j] = bin_it(x=m[0,:],y=zobs[ind])
         zdist_flux_cuts_scatter[j] = bin_it_scatter(x=m[0,:],y=zobs[ind])
+
+    
+    for j, i in enumerate(selec_alma):
+        for tn,t in enumerate(flux_threshs_compactab):
+            ind = np.where((SEDs_dust[i,:] > 0) & (SEDs_dust[i,:] < t))
+            SEDs_dust_smgs = SEDs_dust[:,ind]
+            SEDs_dust_smgs = SEDs_dust_smgs[:,0,:]
+            z_smgs = zobs[ind]
+            ms_smgs = np.log10((msb[ind] + msd[ind])/h0) 
+            ssfr_smgs = np.log10((sfrb[ind] + sfrd[ind])/(msb[ind] + msd[ind]))
+            mhalo_smgs = np.log10(mhalo[ind]/h0)
+            props_vs_z_flux_cuts[j,0,tn,:] = bin_it_z(x=z_smgs, y=ms_smgs)
+            props_vs_z_flux_cuts[j,1,tn,:] = bin_it_z(x=z_smgs, y=ssfr_smgs)
+            props_vs_z_flux_cuts[j,2,tn,:] = bin_it_z(x=z_smgs, y=mhalo_smgs)
+
+            for bn, b in enumerate(bands_of_interest_for_smgs):
+                 ind = np.where((SEDs_dust_smgs[b,:] > 0) & (SEDs_dust_smgs[b,:] < 40))
+                 H, bins_edges = np.histogram(SEDs_dust_smgs[b,ind],bins=np.append(mbinsab,muppab))
+                 ncounts_optical_ir_smgs[j,bn,tn,:] = ncounts_optical_ir_smgs[j,bn,tn,:] + H
+                 mags_selec = SEDs_dust_smgs[b,ind]
+                 mags_vs_z_flux_cuts[j,bn,tn,:] = bin_it_z(x=z_smgs[ind], y=mags_selec[0])
+
+    for bn, b in enumerate(bands_of_interest_for_smgs):
+        ind = np.where((SEDs_dust[b,:] > 0) & (SEDs_dust[b,:] < 40))
+        H, bins_edges = np.histogram(SEDs_dust[b,ind],bins=np.append(mbinsab,muppab))
+        ncounts_all[bn,:] = ncounts_all[bn,:] + H
+
+    ind = np.where(((msb+msd)/h0 > 3e9) & ((msb+msd)/h0 < 1e10))
+    ms_z[:] = bin_it_z(x=zobs[ind], y=np.log10((sfrb[ind] + sfrd[ind])/(msb[ind] + msd[ind])))
 
 def main():
 
@@ -228,7 +489,7 @@ def main():
 
     Variable_Ext = True
     sed_file = "Sting-SED-eagle-rr14-testmmbands"
-    subvols = (0,1) #range(20) #(40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63) 
+    subvols = (0,1) #,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63) #(0,1) #range(20) #(40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63) 
     #0,1,2,3,4,5,6,7,8,9,10,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63) # #(0,10,11,12,13,14,15,16,17) #2,3,4) #range(64) 
 
     # Loop over redshift and subvolumes
@@ -246,7 +507,8 @@ def main():
     ids_sed, seds = common.read_photometry_data_hdf5(lightcone_dir, fields_sed, subvols, sed_file)
  
     fields = {'galaxies': ('dec', 'ra', 'zobs',
-                           'id_galaxy_sky')}
+                           'id_galaxy_sky', 'mstars_bulge', 'mstars_disk', 
+                           'mvir_hosthalo', 'sfr_burst', 'sfr_disk', 'type')}
 
     hdf5_data = common.read_lightcone(lightcone_dir, fields, subvols)
 
@@ -254,12 +516,52 @@ def main():
     zdist_flux_cuts = np.zeros(shape = (len(bands), 2,len(flux_threshs)))
     zdist_flux_cuts_scatter = np.zeros(shape = (len(bands), 3,len(flux_threshs)))
 
-    prepare_data(seds, ids_sed, hdf5_data, subvols, lightcone_dir, nbands, bands, zdist_flux_cuts, zdist_flux_cuts_scatter)
+
+   #(0): "FUV_GALEX", "NUV_GALEX", "u_SDSS", "g_SDSS", "r_SDSS", "i_SDSS",
+   #(6): "z_SDSS", "Y_VISTA", "J_VISTA", "H_VISTA", "K_VISTA", "W1_WISE",
+   #(12): "I1_Spitzer", "I2_Spitzer", "W2_WISE", "I3_Spitzer", "I4_Spitzer",
+   #(17): "W3_WISE", "W4_WISE", "P70_Herschel", "P100_Herschel",
+   #(21): "P160_Herschel", "S250_Herschel", "S350_Herschel", "S450_JCMT",
+   #(25): "S500_Herschel", "S850_JCMT", "Band9_ALMA", "Band8_ALMA",
+   #(29): "Band7_ALMA", "Band6_ALMA", "Band5_ALMA", "Band4_ALMA"
+
+    #"FUV_GALEX", "NUV_GALEX", "u_SDSS", "g_SDSS", "r_SDSS", "i_SDSS",
+    #"z_SDSS", "Y_VISTA", "J_VISTA", "H_VISTA", "K_VISTA", "W1_WISE",
+    #"W2_WISE", "W3_WISE", "W4_WISE"
+    bands_of_interest_for_smgs = (0,1,2,3,4,5,6,7,8,9,10,11,14,17,18)
+    selec_alma = (29, 30, 32)
+
+    #define arrays
+    ncounts_optical_ir_smgs = np.zeros(shape = (len(selec_alma), len(bands_of_interest_for_smgs), len(flux_threshs_compact), len(mbinsab)))
+    ncounts_all =  np.zeros(shape = (len(bands_of_interest_for_smgs), len(mbinsab)))
+    mags_vs_z_flux_cuts = np.zeros(shape =  (len(selec_alma), len(bands_of_interest_for_smgs),  len(flux_threshs_compact), 3, len(zbins)))
+    props_vs_z_flux_cuts = np.zeros(shape =  (len(selec_alma), 3,  len(flux_threshs_compact), 3, len(zbins)))
+    ms_z = np.zeros(shape =  (3, len(zbins)))
+
+    #process data
+    prepare_data(seds, ids_sed, hdf5_data, subvols, lightcone_dir, nbands, bands, zdist_flux_cuts, zdist_flux_cuts_scatter,
+                 ncounts_optical_ir_smgs, bands_of_interest_for_smgs, selec_alma, ncounts_all, mags_vs_z_flux_cuts, 
+                 props_vs_z_flux_cuts, ms_z)
+    print ms_z
+    if(totarea > 0.):
+        ncounts_optical_ir_smgs   = ncounts_optical_ir_smgs/areasub/dmab
+        ncounts_all = ncounts_all/areasub/dmab
+
+    # Take logs
+    ind = np.where(ncounts_optical_ir_smgs > 0)
+    ncounts_optical_ir_smgs[ind] = np.log10(ncounts_optical_ir_smgs[ind])
+    ind = np.where(ncounts_all > 0)
+    ncounts_all[ind] = np.log10(ncounts_all[ind])
 
     if(Variable_Ext):
        outdir = os.path.join(outdir, 'eagle-rr14')
 
     plot_redshift(plt, outdir, obsdir, zdist_flux_cuts, zdist_flux_cuts_scatter)
+    plot_number_counts_smgs(plt, outdir, obsdir,  ncounts_optical_ir_smgs, 
+                            bands_of_interest_for_smgs, ncounts_all)
+    plot_magnitudes_z_smgs(plt, outdir, obsdir, mags_vs_z_flux_cuts)
+    plot_props_z_smgs(plt, outdir, obsdir, props_vs_z_flux_cuts, ms_z)
+
 
 if __name__ == '__main__':
     main()
